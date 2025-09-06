db0a3a8055b91d530f6b6efcbdce49ad9522f2c0
qetiproject <ketideveloper@gmail.com>
2025-09-06 12:31:01 +0400

    search

--- src/app/features/books/pages/book-list/book-list.component.ts
+++ src/app/features/books/pages/book-list/book-list.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, signal, WritableSignal } from '@angular/core';
+import { Component, effect, inject, signal, WritableSignal } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { catchError, of } from 'rxjs';
+import { catchError, of, Subscription } from 'rxjs';
 import { DebounceSearchComponent } from "../../../../components/debounce-search/debounce-search.component";
 import { BookService } from '../../services/book.service';
 import { SearchBooksView } from '../../types/book';
@@ -16,53 +16,35 @@ import { SearchBooksView } from '../../types/book';
 export class BookListComponent{
 
   private bookService = inject(BookService);
-  // searchControl = new FormControl('');
 
-  // // 1️⃣ query სიგნალი (writable)
-  // query: WritableSignal<string> = signal('');
-
-  // // 2️⃣ debounce
-  // debouncedQuery = debouncedSignal(this.query, 400);
-
-  // 3️⃣ შედეგები
   results: WritableSignal<SearchBooksView[]> = signal([]);
+  searchQuery = signal<string>('');
+  #sub: Subscription | null = null;
 
-  // constructor() {
-  //   // FormControl → Signal binding
-  //   this.searchControl.valueChanges.subscribe(value => {
-  //     this.query.set(value?.trim() || '');
-  //   });
-
-  //   // Effect რეაგირებს debouncedQuery-ზე
-  //   effect((onCleanup) => {
-  //     const q = this.debouncedQuery();
-
-      // if (!q || q.length < 3) {
-      //   this.results.set([]);
-      //   return;
-      // }
-
-      // const sub = this.bookService.searchBooksByName(q).pipe(
-      //   catchError(() => of([]))
-      // ).subscribe(res => this.results.set(res));
-
-  //     onCleanup(() => sub.unsubscribe());
-  //   });
-  // }
-
-  onCategorySelectedEvent(value: string | null) {
-    console.log(value, "value")
-  }
-  
-
-  onSearchEvent(value: string) {
-      if (!value || value.length < 3) {
+  constructor() {
+    // Effect რეაგირებს debouncedQuery-ზე
+    effect((onCleanup) => {
+      const query = this.searchQuery();
+      if (!query || query.length < 3) {
         this.results.set([]);
         return;
       }
-      this.bookService.searchBooksByName(value).pipe(
-        catchError(() => of([]))
-      ).subscribe(res => this.results.set(res));
+      // ჩაკეტე წინა subscription, თუ არსებობს
+      if (this.#sub) {
+        this.#sub.unsubscribe();
+      }
+      this.#sub = this.bookService.searchBooksByName(query).pipe(catchError(() => of([])))
+        .subscribe(res => this.results.set(res));
+      // Cleanup on next effect run
+      onCleanup(() => this.#sub?.unsubscribe());
+    });
+  }
 
+  onSearchEvent(value: string) {
+    this.searchQuery.set(value);
+  }
+
+  onCategorySelectedEvent(value: string | null) {
+    console.log(value, "value")
   }
 }
