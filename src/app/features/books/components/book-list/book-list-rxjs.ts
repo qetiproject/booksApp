cbe37eaf183f545941393deb5273f93d4ad333c6
qetiproject <ketideveloper@gmail.com>
2025-09-08 16:19:25 +0400

    advanced load books with signals

--- src/app/features/books/components/book-list/book-list-rxjs.ts
+++ src/app/features/books/components/book-list/book-list-rxjs.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, computed, effect, inject, input, signal, WritableSignal } from '@angular/core';
+import { Component, inject, input, signal, WritableSignal } from '@angular/core';
 import { takeUntilDestroyed, toObservable } from '@angular/core/rxjs-interop';
 import { ReactiveFormsModule } from '@angular/forms';
 import { catchError, of, switchMap } from 'rxjs';
@@ -9,7 +9,7 @@ import { BooksView } from '../../types/book';
 import { BookCardComponent } from '../book-card/book-card.component';
 
 @Component({
-  selector: 'app-book-list',
+  selector: 'app-book-list-rxjs',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule, BookCardComponent],
   templateUrl: './book-list.component.html',
@@ -29,52 +29,12 @@ export class BookListComponent {
 
   // result
   searchedBooks: WritableSignal<BooksView[]> = signal([]);
-  booksByCategory: WritableSignal<BooksView[]> = signal([]);
-  bookToShow = computed(() => {
-    const combined = [
-      ...this.searchedBooks(),
-      ...this.booksByCategory()
-    ];
-    const uniqueBooksMap = new Map<string, BooksView>();
-    combined.forEach(book => uniqueBooksMap.set(book.id, book));
-    return Array.from(uniqueBooksMap.values());
-  });
+  loadBooksByCategory: WritableSignal<BooksView[]> = signal([]);
+  bookToShow: WritableSignal<BooksView[]> = signal([]);
 
   constructor() {
-    this.initEffects();
-  }
-
-  initEffects() {
-    effect(() => {
-      const query = this.searchQuery();
-      if(!query || query.length >= 3) {
-        this.searchedBooks.set([]);
-        return;
-      }
-      this.bookService.searchBooksByName(query)
-        .pipe(
-          catchError(() => {
-            this.messageService.showMessage('Error searching books', 'error');
-            return of([]);
-        }))
-        .subscribe(result => this.searchedBooks.set(result));
-    })
-
-    effect(() => {
-      const category = this.categorySelected();
-      if(!category) {
-        this.booksByCategory.set([]);
-        return;
-      }
-      this.bookService.loadBooksByCategory(category)
-        .pipe(
-            catchError(() => {
-              this.messageService.showMessage('Error loading books by category', 'error');
-            return of([]);
-            })
-        ).subscribe(result => this.booksByCategory.set(result));
-
-    })
+    this.searchData();
+    this.booksByCategory();
   }
 
   private searchData(): void{
@@ -124,6 +84,17 @@ export class BookListComponent {
         this.loadBooksByCategory.set(result);
         this.updateBooksToShow(result);
     })
+  }
+
+  updateBooksToShow(lastFetched: BooksView[]): void {
+    const combined: BooksView[] = [
+      ...lastFetched,
+      ...this.searchedBooks(),
+      ...this.loadBooksByCategory()
+    ]
+    const uniqueBooksMap = new Map<string, BooksView>();
+    combined.forEach(book => uniqueBooksMap.set(book.id, book));
+    this.bookToShow.set(Array.from(uniqueBooksMap.values()))
   }
 
 }
