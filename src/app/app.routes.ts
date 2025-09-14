b28b12f4fc3902111bd8526d2bd922eef3b17859
qetiproject <ketideveloper@gmail.com>
2025-09-14 18:48:04 +0400

    login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { bookRoutes } from './features/books/book.router';
 import { catalogueRoutes } from './pages/catalogues/catalogue.route';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 import { WishlistComponent } from './pages/wishlist/wishlist.component';
 
 export const routes: Routes = [
@@ -9,8 +10,12 @@ export const routes: Routes = [
     path: 'home', 
     component: HomeComponent
   },      
+  {
+    path: 'login',
+    component: LoginComponent
+  },
   { path: '', 
-    redirectTo: 'home', 
+    redirectTo: 'login', 
     pathMatch: 'full' 
   }, 
   {
@@ -25,6 +30,7 @@ export const routes: Routes = [
     path: "favourites",
     component: WishlistComponent
   },
+
   {
     path: '**',
     redirectTo: ''
