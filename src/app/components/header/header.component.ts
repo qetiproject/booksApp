75c006bdc8f1ba9fb273a64bba0210701ee94c46
qetiproject <ketideveloper@gmail.com>
2025-09-15 14:08:29 +0400

    header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,3 +1,4 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, inject } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { logout, userProfile } from '../../features/auth/store/auth.action';
@@ -7,7 +8,19 @@ import { logout, userProfile } from '../../features/auth/store/auth.action';
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
+   animations: [
+    trigger('dropdown', [
+      state('open', style({ opacity: 1, transform: 'scale(1)' })),
+      state('closed', style({ opacity: 0, transform: 'scale(0.95)' })),
+      transition('closed => open', [
+        animate('150ms ease-out')
+      ]),
+      transition('open => closed', [
+        animate('100ms ease-in')
+      ])
+    ])
+  ]
 })
 export class HeaderComponent {
   store = inject(Store);
