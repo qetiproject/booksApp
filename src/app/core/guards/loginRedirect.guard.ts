eabca85eba5309e347b801d64f144e3df3f6519e
qetiproject <ketideveloper@gmail.com>
2025-10-10 19:53:42 +0400

    lazy loading

--- src/app/core/guards/loginRedirect.guard.ts
+++ src/app/core/guards/loginRedirect.guard.ts
@@ -1,7 +1,7 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router, UrlTree } from '@angular/router';
+import { selectIsLoggedIn } from '@auth-module/store/auth.selector';
 import { Store } from '@ngrx/store';
-import { selectIsLoggedIn } from 'modules/auth/store/auth.selector';
 import { Observable } from 'rxjs/internal/Observable';
 import { map, take } from 'rxjs/operators';
 
